12770e8493dec8ab41016096452204193606debf
t-yagit <62111840+t-yagit@users.noreply.github.com>
2022-10-16 22:36:37 +0900

    set stable to docker version.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ sudo apt-get install \
     gnupg \
     lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 # apt-cache madison docker-ce
 sudo apt-get install docker-ce=5:20.10.18~3-0~ubuntu-focal docker-ce-cli=5:20.10.18~3-0~ubuntu-focal containerd.io docker-compose-plugin
