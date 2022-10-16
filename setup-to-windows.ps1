f2454a61a3ae487e3f8deb97ead44fbb5bc3243d
t-yagit <62111840+t-yagit@users.noreply.github.com>
2022-10-17 00:38:01 +0900

    include package for chocolatey.

--- setup-to-windows.ps1
+++ setup-to-windows.ps1
@@ -1,4 +1,4 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-choco install docker-engine docker-cli docker-compose 
+choco install package.config 
 [System.Environment]::SetEnvironmentVariable("DOCKER_HOST", "tcp://localhost:2375", "User")
 cp startup.bat $([System.Environment]::GetFolderPath("Startup"))
