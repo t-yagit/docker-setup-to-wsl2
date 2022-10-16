cf981c5f8c1eaef92fdcace24be33ec5b934178e
t-yagit <62111840+t-yagit@users.noreply.github.com>
2022-10-16 22:24:31 +0900

    add inital setup code.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo apt-get update
 # apt-cache madison docker-ce
