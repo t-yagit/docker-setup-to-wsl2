44e1ac35cb221e9fae201dc36a54089199089626
t-yagit <62111840+t-yagit@users.noreply.github.com>
2022-10-17 00:39:15 +0900

    fixed installed code for chocolatey.

--- setup-to-windows.ps1
+++ setup-to-windows.ps1
@@ -1,4 +1,4 @@
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install package.config 
 [System.Environment]::SetEnvironmentVariable("DOCKER_HOST", "tcp://localhost:2375", "User")
 cp startup.bat $([System.Environment]::GetFolderPath("Startup"))
